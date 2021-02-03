eb8b31b0ea38ac16cf18e60301e56c526166b574
Rendy Artha <rendyartha312@gmail.com>
2021-02-03 16:41:53 +0700

    done

--- index.js
+++ index.js
@@ -21,3 +21,37 @@ function factorial(n) {
 console.log('Soal2', factorial(3))
 
 // latihan 3
+function rekursive(n) {
+  if(n == 1) {
+    return 3
+  } else {
+    return 2 * rekursive(n-1)
+  }
+}
+console.log('Soal3', rekursive(3))
+
+// latihan 4
+function dibagiLimaBelas(n) {
+  if(n < 1) {
+    return 0 
+  } else {
+    return 1 + dibagiLimaBelas(n-15)
+  }
+}
+
+console.log('Soal4', dibagiLimaBelas(1))
+
+// latihan 5
+let arr = []
+
+function recArray(number, times) {
+  if(number < 1) {
+    return [];
+  } else {
+    arr.push(times)
+    recArray(number - 1, times);
+    return arr
+  }
+}
+
+console.log('Soal5', recArray(0))
